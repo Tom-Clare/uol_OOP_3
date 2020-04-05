28574f87bbafdd44ec3437b5996f513ed488ba86
Tom Clare <tclare5@gmail.com>
2020-04-05 22:43:00 +0100

    New test solution

---
+++ uol_OOP_3_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("uol_OOP_3_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("uol_OOP_3_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4639fcbc-4ec7-400a-9c23-59595603c1d5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ uol_OOP_3_Tests/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace uol_OOP_3_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static void clearStringWriter(StringWriter sw)
+        {
+            StringBuilder sb = sw.GetStringBuilder();
+            sb.Remove(0, sb.Length);
+        }
+
+        [TestMethod]
+        public void Test_Identical_Files()  // Large test covers the a to z of the program.
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);  // Catch output
+
+                uol_OOP_3.Program.Equate("diff GitRepositories_1a.txt GitRepositories_1b.txt");  // Provided files are identical
+                string result = sw.ToString().Trim();
+                string expected = "GitRepositories_1a.txt and GitRepositories_1b.txt are the same";
+                Assert.AreEqual(expected, result);
+            }
+        }
+
+        [TestMethod]
+        public void Test_Differing_Files()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);  // Catch output
+
+                uol_OOP_3.Program.Equate("diff GitRepositories_1a.txt GitRepositories_2a.txt");  // Provided files are not identical
+                string result = sw.ToString().Trim();
+                string expected = "GitRepositories_1a.txt and GitRepositories_2a.txt are not the same";  // Might change as development progresses.
+                Assert.AreEqual(expected, result);
+            }
+        }
+    }
+}
